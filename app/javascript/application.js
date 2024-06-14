c41be307f621b30db6168fe14a2a661b306df01a
Leland Reardon <leland6925@gmail.com>
2024-06-14 16:18:49 -0700

    .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "../assets/builds/tailwind.css"
 import "./channels"
 import "./config"
 import "./controllers"
