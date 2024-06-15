3c74609a1b1a71f8c09cb4448c6142e4c61b5ab8
Leland Reardon <leland6925@gmail.com>
2024-06-15 14:56:34 -0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,10 @@ Rails.application.configure do
 	# config.action_cable.mount_path = nil
 	# config.action_cable.url = "wss://example.com/cable"
 	# config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-	config.action_cable.logger = :debug
+	# config.action_cable.logger = :debug
 
 	# Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
 	# config.force_ssl = true
-
 
 	# Use a different cache store in production.
 	# config.cache_store = :mem_cache_store
