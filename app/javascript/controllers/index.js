860d74f5e32fab74e74348ccc39ba94cb8f7d1ed
Leland Reardon <leland6925@gmail.com>
2024-06-14 17:32:23 -0700

    .

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,3 +5,6 @@ import controllers from "./**/*_controller.js"
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default)
 })
+
+StimulusReflex.initialize(application)
+StimulusReflex.debug = process.env.RAILS_ENV === 'development'
