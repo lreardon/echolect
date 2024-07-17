d74be51a10a5ef1848b2a378cd3f7cbc983a5470
Leland Reardon <leland6925@gmail.com>
2024-07-17 11:24:05 -0700

    .

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-	# before_action :configure_sign_in_params, only: [:create]
+	before_action :clear_flash_messages, only: %i[create new]
 
 	# GET /resource/sign_in
 	# def new
@@ -24,4 +24,10 @@ class Users::SessionsController < Devise::SessionsController
 	# def configure_sign_in_params
 	#   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
 	# end
+
+	private
+
+	def clear_flash_messages
+		flash.clear
+	end
 end
