70f9e15cd835210d04a01ca0d6de51dff53bd7e9
Leland Reardon <leland6925@gmail.com>
2024-07-17 09:46:58 -0700

    .

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-	default from: 'notifications@example.com'
+	default from: 'postmaster@echolect.co'
 
 	def welcome_email
 		@user = params[:user]
